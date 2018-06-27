21fb0e49b642b7c47653e92d417f5615f96aa3d2
cdmccauley <cdmccauley@gmail.com>
2018-06-26 21:14:06 -0600

    AndroidHanaDulSes started

---
+++ AndroidHanaDulSes/app/src/main/java/tech/mccauley/androidhanadulses/SplashActivity.java
@@ -0,0 +1,41 @@
+package tech.mccauley.androidhanadulses;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // set task
+        TimerTask splashTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                // start mainactivity
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        };
+
+        // set timer
+        Timer splashTimer = new Timer();
+        splashTimer.schedule(splashTimerTask, 3000);
+
+        // get frames
+        ImageView splashAnimationIv = findViewById(R.id.splash_animation_iv);
+        AnimationDrawable framesXml = (AnimationDrawable)splashAnimationIv.getBackground();
+
+        // start animation
+        framesXml.start();
+
+    }
+}
